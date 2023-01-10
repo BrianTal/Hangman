8d661e27aa32ab98c6fc471af7009169398ba023
Brian <talbertech@gmail.com>
2023-01-09 21:51:36 -0500

    Add check_guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,10 +1,12 @@
 class Hangman
   def initialize()
     @word = get_word
+    @word_array = @word.split('')
     @guesses_remaining = 7
-    @solution = []
+    @solution_display = []
     create_solution_display
     @guesses = []
+    @game_over = false
   end
 
   def get_word
@@ -18,17 +20,19 @@ class Hangman
 
   def create_solution_display
     ch_array = @word.split('')
-    ch_array.each { @solution.push("_") }
+    ch_array.each { @solution_display.push("_") }
   end
 
   def play_game
     display_game_info
     current_guess = get_guess
+    check_guess(current_guess)
+    self.win?
   end
   
   def display_game_info
     puts @word
-    puts @solution.join(" ")
+    puts @solution_display.join(" ")
     puts "You have #{@guesses_remaining} guesses remaining"
     guess_string = @guesses.join(",")
     puts "Your previous guesses were: #{@guesses.join(',')}"
@@ -41,10 +45,30 @@ class Hangman
       if @guesses.include?(guess)
         puts "You've already entered that guess."
       end
-      puts "Please enter a valid single letter a through z:"
+      puts "Please enter a valid single letter a through z that was not guessed before:"
       guess = gets.chomp.downcase
     end
-    puts guess
+    @guesses_remaining -= 1
+    @guesses.push(guess)
+    return guess
+  end
+
+  def check_guess(guess)
+    if @word_array.include?(guess)
+      puts "Good Choice!"
+      @word_array.each_with_index do | ch, index |
+        if ch == guess
+          @solution_display[index] = ch
+        end
+      end
+      puts @solution_display.join(" ")
+    else
+      puts "Sorry the word does not contain that letter!"
+    end
+  end
+
+  def win?
+    puts "did we win?"
   end
 end
 
