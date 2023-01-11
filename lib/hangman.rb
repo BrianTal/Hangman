f21ba9e6b297f3aacb7e47fbb8378792ebeafe87
Brian <talbertech@gmail.com>
2023-01-10 20:58:45 -0500

    Working on Load

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Hangman
   def initialize()
     @word = get_word
@@ -27,6 +29,10 @@ class Hangman
     until @game_over || @guesses_remaining < 1 do
       display_game_info
       current_guess = get_guess
+      if current_guess == "save"
+        save
+        exit 
+      end
       check_guess(current_guess)
       @game_over = self.win?
     end
@@ -47,8 +53,9 @@ class Hangman
   end
 
   def get_guess
-    print "Enter your next letter: "
+    print "Enter your next letter or 'save' to save game: "
     guess = gets.chomp.downcase
+    return guess if guess == "save"
     until guess.length == 1 && (guess.ord > 96 && guess.ord < 123) && !(@guesses.include?(guess))
       if @guesses.include?(guess)
         puts "You've already entered that guess!"
@@ -77,8 +84,46 @@ class Hangman
   def win?
     @word_array == @solution_display
   end
+
+  def save
+    Dir.mkdir('./game_data') unless Dir.exist?('./game_data')
+    print "Enter file name: "
+    filename = gets.chomp
+    File.open("./game_data/#{filename}.yml", 'w') { |file| YAML.dump([] << self, file) }
+    puts "Thank you for playing"
+  end
+
+  def load_game
+    game_name = get_game_name
+    puts "before"
+    game_yaml = YAML.load_file("./game_data/#{game_name}.yml")
+    puts "after"
+    p game_yaml
+    @word = game_yaml.word
+    @word_array = game_yaml.word_array
+    @guesses_remaining = game_yaml.guesses_remaining
+    @solution_display = game_yaml.solution_display
+    @guesses = game_yaml.guesses
+  end
+
+  def get_game_name
+    display_saved_games
+    print "What game would you like to load: "
+    game = gets.chomp
+  end
+
+  def display_saved_games
+    puts "You have the following saves:"
+    saved_games = Dir.glob('./game_data/*')
+    saved_games.each do |game|
+      print "#{game.split('/')[2]} "
+    end
+    puts ""
+  end
+
 end
 
 game = Hangman.new
+game.load_game
 game.play_game
 p game
